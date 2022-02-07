9e4c7a41cc21733ccb23c65e423d4d1f42d3bd6a
Nasirahmadnoori <nasirahmad.noori222@gmail.com>
2022-02-07 15:52:32 +0430

    Add new changes

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:jobs/views/job_page.dart';
+
+void main() {
+  runApp(const JobsPage());
+}
---
+++ lib/views/job_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JobsPage extends StatefulWidget {
+  const JobsPage({Key? key}) : super(key: key);
+
+  @override
+  _JobsPageState createState() => _JobsPageState();
+}
+
+class _JobsPageState extends State<JobsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
